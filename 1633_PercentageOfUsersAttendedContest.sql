6e3f5e903fb56c4cd69a21245b766d24012f9f62
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-01 14:22:21 +0530

    sql-50_1.18

--- 1633_PercentageOfUsersAttendedContest.sql
+++ 1633_PercentageOfUsersAttendedContest.sql
@@ -1,3 +1,47 @@
 
 -- Problem link : https://leetcode.com/problems/percentage-of-users-attended-a-contest/description/?envType=study-plan-v2&envId=top-sql-50
 
+-- Create the Users table
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    user_name VARCHAR(255)
+);
+
+-- Insert values into the Users table
+INSERT INTO Users (user_id, user_name) VALUES
+(6, 'Alice'),
+(2, 'Bob'),
+(7, 'Alex');
+
+-- Create the Register table
+CREATE TABLE Register (
+    contest_id INT,
+    user_id INT,
+    PRIMARY KEY (contest_id, user_id)
+);
+
+-- Insert values into the Register table
+INSERT INTO Register (contest_id, user_id) VALUES
+(215, 6),
+(209, 2),
+(208, 2),
+(210, 6),
+(208, 6),
+(209, 7),
+(209, 6),
+(215, 7),
+(208, 7),
+(210, 2),
+(207, 2),
+(210, 7);
+
+
+SELECT * FROM Users;
+
+SELECT * FROM Register;
+
+-- main query
+SELECT contest_id, ROUND((COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT(user_id) FROM Users)), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
